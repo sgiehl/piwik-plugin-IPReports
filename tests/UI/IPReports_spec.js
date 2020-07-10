db6900cb21e8155424b3f3c7636406a4381e4d70
sgiehl <stefan@matomo.org>
2020-07-10 13:19:47 +0200

    convert UI tests to headless chrome

--- tests/UI/IPReports_spec.js
+++ tests/UI/IPReports_spec.js
@@ -1,7 +1,7 @@
 /*!
  * Matomo - free/libre analytics platform
  *
- * Screenshot tests for ReferrersManager plugin
+ * Screenshot tests for IPReports plugin
  *
  * @link https://matomo.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
@@ -10,9 +10,16 @@
 describe("IPReports", function () {
     this.timeout(0);
 
-    it('show report', function (done) {
-        expect.screenshot('report').to.be.captureSelector('#widgetIPReportsgetIPs', function (page) {
-            page.load('?module=CoreHome&action=index&idSite=1&period=day&date=2012-08-09#?idSite=1&period=month&date=2012-08-09&category=General_Visitors&subcategory=UserCountry_SubmenuLocations');
-        }, done);
+    this.fixture = "Piwik\\Plugins\\IPReports\\tests\\Fixtures\\IPFixture";
+
+    before(function () {
+        testEnvironment.pluginsToLoad = ['IPReports'];
+        testEnvironment.save();
+    });
+
+    it('show report', async function () {
+        await page.goto('?module=CoreHome&action=index&idSite=1&period=day&date=2014-09-04#?idSite=1&period=month&date=2014-09-04&category=General_Visitors&subcategory=UserCountry_SubmenuLocations');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector('#widgetIPReportsgetIPs')).to.matchImage('report');
     });
 });
